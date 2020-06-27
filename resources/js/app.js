07eb296b9b21e34e55f8d7d8a408f641ae945035
Graham Still <gstill@uw.edu>
2020-06-26 22:27:34 -0500

    Add turbolinks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
 require('./bootstrap');
+const Turbolinks = require('turbolinks');
+const TurbolinksAdapter = require('vue-turbolinks');
+Turbolinks.start();
 
 window.Vue = require('vue');
 const Vuex = require('vuex');
 
 Vue.use(Vuex);
+Vue.use(TurbolinksAdapter);
 
 const store = new Vuex.Store({
   state: {
@@ -21,7 +25,9 @@ Vue.component('alglang-group', require('./components/Group.vue').default);
 Vue.component('alglang-language', require('./components/Language.vue').default);
 Vue.component('alglang-map', require('./components/Map.vue').default);
 
-const app = new Vue({
-  el: '#app',
-  store
+document.addEventListener('turbolinks:load', () => {
+  new Vue({
+    el: '#app',
+    store
+  });
 });
