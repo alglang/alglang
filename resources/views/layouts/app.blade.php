3c24c2be8eb1133cd48bb183448b686e59c92ae1
Graham Still <gstill@uw.edu>
2020-06-26 22:08:19 -0500

    Update site title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Database of Algonquian Language Structures</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
