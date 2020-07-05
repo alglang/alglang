92e7463cd8a2879b10df4e7a750fbf50b5289006
Graham Still <gstill@uw.edu>
2020-07-05 13:27:22 -0500

    Set custom 403 page

--- tests/Feature/ViewErrorsTest.php
+++ tests/Feature/ViewErrorsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,6 +16,17 @@ class ViewErrorsTest extends TestCase
     {
         $response = $this->get('/fhjklasdhfjkasldf');  // Non-existent route
         $response->assertStatus(404);
+
+        $response->assertSee(config('app.admin_email'));
+    }
+
+    /** @test */
+    public function error_403_contains_a_contact_email()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get('/languages/create');  // Protected route
+        $response->assertStatus(403);
 
         $response->assertSee(config('app.admin_email'));
     }
