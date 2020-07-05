b152b113898e88b8f97d79b2b334ff67552264dd
Graham Still <gstill@uw.edu>
2020-07-05 00:02:38 -0500

    Add dropdown menu for logging in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,9 +85,22 @@
                 </a>
 
                 @guest
-                <a href="{{ route('login') }}" class="flex items-center px-3 h-full uppercase tracking-wide bg-yellow-400 text-gray-900 hover:bg-yellow-500 hover:text-gray-900">
-                    <span>Log in</span>
-                </a>
+                <div class="relative group h-full">
+                    <a href="{{ route('login') }}" class="flex items-center px-3 h-full uppercase tracking-wide bg-yellow-400 text-gray-900 hover:bg-yellow-500 hover:text-gray-900">
+                        <span>Log in</span>
+                    </a>
+
+                    <ul class="absolute right-0 border-1 border-gray-800 bg-gray-100 py-4 px-6 hidden group-hover:block shadow-lg">
+                        <li>
+                            <a href="{{ route('auth', ['provider' => 'github']) }}" class="flex items-center p-2 text-gray-100 bg-gray-800 hover:text-gray-100 hover:bg-gray-700">
+                                <svg class="w-8 p-1 fill-current border-r border-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.18 11.309c-.718 0-1.3.807-1.3 1.799 0 .994.582 1.801 1.3 1.801s1.3-.807 1.3-1.801c-.001-.992-.582-1.799-1.3-1.799zm4.526-4.683c.149-.365.155-2.439-.635-4.426 0 0-1.811.199-4.551 2.08-.575-.16-1.548-.238-2.519-.238-.973 0-1.945.078-2.52.238C4.74 2.399 2.929 2.2 2.929 2.2c-.789 1.987-.781 4.061-.634 4.426C1.367 7.634.8 8.845.8 10.497c0 7.186 5.963 7.301 7.467 7.301l1.734.002 1.732-.002c1.506 0 7.467-.115 7.467-7.301 0-1.652-.566-2.863-1.494-3.871zm-7.678 10.289h-.056c-3.771 0-6.709-.449-6.709-4.115 0-.879.31-1.693 1.047-2.369C5.537 9.304 7.615 9.9 9.972 9.9h.056c2.357 0 4.436-.596 5.664.531.735.676 1.045 1.49 1.045 2.369 0 3.666-2.937 4.115-6.709 4.115zm-3.207-5.606c-.718 0-1.3.807-1.3 1.799 0 .994.582 1.801 1.3 1.801.719 0 1.301-.807 1.301-1.801 0-.992-.582-1.799-1.301-1.799z"/></svg>
+                                <p class="px-2">
+                                    Github
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 @else
                 <a href="#" class="flex items-center bg-yellow-400 text-gray-900 uppercase tracking-wide px-3 h-full uppercase bg-yellow-400 text-gray-900 hover:bg-yellow-500 hover:text-gray-900">
                     <span>Add</span>
