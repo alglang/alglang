cc3b1004487fb6bea50b3d7b5a9b38a9d4da44b5
Graham Still <gstill@uw.edu>
2020-08-03 16:16:30 -0500

    Mock Turbolinks.visit in tests

--- tests/JavaScript/setup.js
+++ tests/JavaScript/setup.js
@@ -2,6 +2,12 @@ require('jsdom-global')('', {
   url: 'https://alglang.net'
 });
 
+window.Turbolinks = {
+  visit: hash => {
+    window.location.hash = hash;
+  }
+};
+
 const chai = require('chai');
 const chaiDom = require('chai-dom');
 
