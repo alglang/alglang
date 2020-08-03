d4e02fdfbd34028b053f44db47b10fa83750880f
Graham Still <gstill@uw.edu>
2020-08-03 13:23:14 -0500

    Add source fetching route

--- app/Http/Controllers/SourceController.php
+++ app/Http/Controllers/SourceController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Source;
+use App\Http\Resources\SourceCollection;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class SourceController extends Controller
 {
-    /** @test */
-    public function show(Source $source): \Illuminate\View\View
+
+    public function fetch(): SourceCollection
+    {
+        $sources = Source::paginate(10);
+        return new SourceCollection($sources);
+    }
+    
+    public function show(Source $source): View
     {
         $source->loadCount('morphemes', 'verbForms');
         return view('sources.show', ['source' => $source]);
