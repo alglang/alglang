f34b3248fa9af26418a11e2fd597b94976e67841
Graham Still <gstill@uw.edu>
2020-08-17 20:36:50 -0500

    Simplify test

--- tests/Unit/NominalFormTest.php
+++ tests/Unit/NominalFormTest.php
@@ -4,8 +4,6 @@ namespace Tests\Unit;
 
 use App\Language;
 use App\NominalForm;
-use App\NominalParadigm;
-use App\NominalStructure;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -20,12 +18,7 @@ class NominalFormTest extends TestCase
         $language = factory(Language::class)->create(['algo_code' => 'PA']);
         $form = factory(NominalForm::class)->create([
             'shape' => 'N-test',
-            'language_id' => $language->id,
-            'structure_id' => factory(NominalStructure::class)->create([
-                'paradigm_id' => factory(NominalParadigm::class)->create([
-                    'language_id' => $language->id
-                ])->id
-            ])->id
+            'language_id' => $language->id
         ]);
         $this->assertEquals('/languages/pa/nominal-forms/N-test', $form->url);
     }
