bf7589fb2ce946a77075a582579a3ff2719eb4a3
Graham Still <gstill@uw.edu>
2020-08-26 22:24:50 -0500

    Only load max 10 languages into the language menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -47,7 +47,7 @@
             </a>
             
             <ul class="absolute md:right-0 py-1 border-t border-gray-100 bg-gray-900 hidden group-hover:block whitespace-no-wrap">
-                @foreach(App\Language::all() as $language)
+                @foreach(App\Language::limit(10)->get() as $language)
                 <li>
                     <a href="{{ $language->url }}" class="block p-2 uppercase tracking-wide text-gray-100 hover:text-gray-100 hover:bg-red-700">
                         {{ $language->name }}
