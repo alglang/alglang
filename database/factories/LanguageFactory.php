3b023578213db8be1ef609a3b41f6a6e4c253f9c
Graham Still <gstill@uw.edu>
2020-09-01 13:40:13 -0500

    Randomize language factory name

--- database/factories/LanguageFactory.php
+++ database/factories/LanguageFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(Language::class, function (Faker $faker) {
     return [
-        'name' => 'Factory Created Language',
+        'name' => $faker->unique()->words(3, true),
         'code' => $faker->unique()->lexify('???'),
         'group_name' => factory(Group::class)
     ];
