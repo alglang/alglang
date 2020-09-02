090842f29e4d14c9710617e3e3e2bf92ce04ea3b
Graham Still <gstill@uw.edu>
2020-09-02 11:30:29 -0500

    Ensure dropdowns take up at least the full space of their handle

--- resources/views/layouts/partials/dropdown-list.blade.php
+++ resources/views/layouts/partials/dropdown-list.blade.php
@@ -1,5 +1,5 @@
 <ul
-    class="absolute group-focus-within:scale-y-100 py-1 w-fit
+    class="absolute group-focus-within:scale-y-100 py-1
            max-h-md overflow-y-auto scrollbar scrollbar-track-gray-800 scrollbar-thumb-yellow-400 hover:scrollbar-thumb-yellow-500
            transform scale-y-0 group-hover:scale-y-100 transition duration-150 ease-in-out origin-top
            border-t border-gray-100 bg-gray-900 whitespace-no-wrap z-50 {{ $class ?? '' }}"
