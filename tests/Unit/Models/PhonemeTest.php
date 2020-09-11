547c879c93c6876669c9435f6abcbb11204f5600
Graham Still <gstill@uw.edu>
2020-09-11 09:52:22 -0500

    Ensure phoneme tests don't actually hit the database

--- tests/Unit/Models/PhonemeTest.php
+++ tests/Unit/Models/PhonemeTest.php
@@ -5,42 +5,42 @@ namespace Tests\Unit\Models;
 use App\Models\Phoneme;
 use App\Models\VowelFeatureSet;
 use App\Models\ConsonantFeatureSet;
-use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class PhonemeTest extends TestCase
 {
     /** @test */
     public function its_slug_is_its_shape_if_it_has_no_slug()
     {
-        $phoneme = Phoneme::factory()->make(['slug' => null, 'shape' => 'x', 'ipa' => 'y']);
+        $phoneme = new Phoneme(['slug' => null, 'shape' => 'x', 'ipa' => 'y']);
         $this->assertEquals('x', $phoneme->slug);
     }
 
     /** @test */
     public function its_slug_is_its_ipa_if_it_has_no_shape()
     {
-        $phoneme = Phoneme::factory()->make(['slug' => null, 'shape' => null, 'ipa' => 'y']);
+        $phoneme = new Phoneme(['slug' => null, 'shape' => null, 'ipa' => 'y']);
         $this->assertEquals('y', $phoneme->slug);
     }
 
     /** @test */
     public function it_has_a_url()
     {
-        $phoneme = Phoneme::factory()->forLanguage(['code' => 'TL'])->make(['shape' => 'x']);
+        $phoneme = new Phoneme(['shape' => 'x', 'language_code' => 'TL']);
         $this->assertEquals('/languages/TL/phonemes/x', $phoneme->url);
     }
 
     /** @test */
     public function its_type_is_vowel_if_it_has_a_vowel_feature_set()
     {
-        $vowel = Phoneme::factory()->make(['featureable_type' => VowelFeatureSet::class]);
+        $vowel = new Phoneme(['featureable_type' => VowelFeatureSet::class]);
         $this->assertEquals('vowel', $vowel->type);
     }
 
     /** @test */
     public function its_type_is_consonant_if_it_has_a_consonant_feature_set()
     {
-        $consonant = Phoneme::factory()->make(['featureable_type' => ConsonantFeatureSet::class]);
+        $consonant = new Phoneme(['featureable_type' => ConsonantFeatureSet::class]);
         $this->assertEquals('consonant', $consonant->type);
     }
 }
--- tests/Unit/Presenters/PhonemePresenterTest.php
+++ tests/Unit/Presenters/PhonemePresenterTest.php
@@ -11,21 +11,24 @@ class PhonemePresenterTest extends TestCase
     /** @test */
     public function it_wraps_its_formatted_shape_in_i_tags()
     {
-        $phoneme = Phoneme::factory()->make(['shape' => 'x']);
+        $phoneme = new Phoneme(['shape' => 'x']);
         $this->assertEquals('<i>x</i>', $phoneme->formatted_shape);
     }
 
     /** @test */
     public function it_marks_its_formatted_shape_as_reconstructed_if_its_language_is_reconstructed()
     {
-        $phoneme = Phoneme::factory()->forLanguage(['reconstructed' => true])->make(['shape' => 'x']);
+        $phoneme = new Phoneme([
+            'shape' => 'x',
+            'language' => new Language(['reconstructed' => true])
+        ]);
         $this->assertEquals('<i>*x</i>', $phoneme->formatted_shape);
     }
 
     /** @test */
     public function it_uses_its_ipa_if_it_has_no_shape()
     {
-        $phoneme = Phoneme::factory()->make(['shape' => null, 'ipa' => 'y']);
+        $phoneme = new Phoneme(['shape' => null, 'ipa' => 'y']);
         $this->assertEquals('<i>y</i>', $phoneme->formatted_shape);
     }
 }
