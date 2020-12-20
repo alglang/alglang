99e785ffdaa9c0d5732db1593401530ba0b45441
Graham Still <gstill@uw.edu>
2020-12-20 16:55:25 -0600

    Load livewire component properties directly
    Trying to be fancy confuses Livewire and prevents it from hydrating
    properly.

--- app/Http/Livewire/Collections/Clusters.php
+++ app/Http/Livewire/Collections/Clusters.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Livewire\Collections;
 
-use App\Models\ClusterFeatureSet;
 use App\Models\Language;
-use App\Models\Phoneme;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -32,35 +29,22 @@ class Clusters extends Component
         $this->clusters = collect();
     }
 
-    public function hydrateClusters(Collection $data): void
-    {
-        $this->clusters = $data->map(function ($data) {
-            $cluster = new Phoneme($data);
-            $cluster['language'] = new Language($data['language']);
-            $cluster['features'] = new ClusterFeatureSet($data['features']);
-            $cluster['features']['firstSegment'] = new Phoneme($data['features']['first_segment']);
-            $cluster['features']['secondSegment'] = new Phoneme($data['features']['second_segment']);
-            return $cluster;
-        });
-    }
-
     public function tabChanged(string $tab): void
     {
         if ($tab === 'clusters' && !$this->loaded) {
+            $this->clusters = $this->model->clusters;
+
             $paFound = false;
 
-            foreach ($this->model->phonoids()->with('features')->get() as $phonoid) {
-                if ($phonoid->is_cluster) {
-                    $this->clusters->push($phonoid);
-                }
-
+            foreach ($this->model->phonoids as $phonoid) {
                 if (!$paFound && ($phonoid->is_cluster || $phonoid->is_consonant) && $phonoid->parentsFromLanguage('PA')->some(fn ($parent) => $parent->is_cluster)) {
-                    $this->paClusters = Language::find('PA')->clusters->where('is_archiphoneme', false);
+                    $this->paClusters = Language::find('PA')->clusters;
                     $paFound = true;
                 }
             }
 
             $this->loaded = true;
+
         }
     }
 
--- app/Http/Livewire/Collections/Phonemes.php
+++ app/Http/Livewire/Collections/Phonemes.php
@@ -2,14 +2,7 @@
 
 namespace App\Http\Livewire\Collections;
 
-use App\Models\ConsonantFeatureSet;
-use App\Models\ConsonantManner;
-use App\Models\ConsonantPlace;
 use App\Models\Language;
-use App\Models\Phoneme;
-use App\Models\VowelBackness;
-use App\Models\VowelFeatureSet;
-use App\Models\VowelHeight;
 use Illuminate\Support\Collection;
 use Illuminate\View\View;
 use Livewire\Component;
@@ -44,49 +37,11 @@ class Phonemes extends Component
         'tabChanged'
     ];
 
-    /**
-     * @param Language $model
-     */
-    public function mount($model): void
+    public function mount(): void
     {
-        $this->model = $model;
         $this->vowels = collect();
         $this->consonants = collect();
         $this->archiphonemes = collect();
-    }
-
-    public function hydrate(): void
-    {
-        $this->vowels = $this->vowels->map(function ($data) {
-            $vowel = new Phoneme($data);
-            $vowel['language'] = new Language($data['language']);
-            $vowel['features'] = new VowelFeatureSet($data['features']);
-            $vowel['features']['backness'] = new VowelBackness($data['features']['backness']);
-            $vowel['features']['height'] = new VowelHeight($data['features']['height']);
-            return $vowel;
-        });
-
-        $this->consonants = $this->consonants->map(function ($data) {
-            $consonant = new Phoneme($data);
-            $consonant['language'] = new Language($data['language']);
-            $consonant['features'] = new ConsonantFeatureSet($data['features']);
-            $consonant['features']['manner'] = new ConsonantManner($data['features']['manner']);
-            $consonant['features']['place'] = new ConsonantPlace($data['features']['place']);
-            return $consonant;
-        });
-
-        $this->archiphonemes = $this->archiphonemes->map(function ($data) {
-            $archiphoneme = new Phoneme($data);
-            $archiphoneme['language'] = new Language($data['language']);
-
-            if ($archiphoneme->is_consonant) {
-                $archiphoneme['features'] = new ConsonantFeatureSet($data['features']);
-            } elseif ($archiphoneme->is_vowel) {
-                $archiphoneme['features'] = new VowelFeatureSet($data['features']);
-            }
-
-            return $archiphoneme;
-        });
     }
 
     public function tabChanged(string $tab): void
@@ -94,15 +49,9 @@ class Phonemes extends Component
         if ($tab === 'phonemes' && !$this->loaded) {
             $phonoids = $this->model->phonoids;
 
-            foreach ($phonoids as $phonoid) {
-                if ($phonoid->is_archiphoneme) {
-                    $this->archiphonemes->push($phonoid);
-                } elseif ($phonoid->is_vowel) {
-                    $this->vowels->push($phonoid);
-                } elseif ($phonoid->is_consonant) {
-                    $this->consonants->push($phonoid);
-                }
-            }
+            $this->archiphonemes = $phonoids->where('is_archiphoneme', true);
+            $this->consonants = $this->model->consonants->where('is_archiphoneme', false);
+            $this->vowels = $this->model->vowels->where('is_archiphoneme', false);
 
             if ($this->model->code !== 'PA') {
                 $pa = Language::find('PA');
@@ -119,7 +68,6 @@ class Phonemes extends Component
                     $this->paArchiphonemes = $pa->phonemes->where('is_archiphoneme', true);
                 }
             }
-
 
             $this->loaded = true;
         }
